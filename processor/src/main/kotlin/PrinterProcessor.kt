0e2943eee61573b4f18792b97872fb970afc3441
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-11-03 16:12:31 +0100

    feat(processor): created symbol processor

--- processor/src/main/kotlin/PrinterProcessor.kt
+++ processor/src/main/kotlin/PrinterProcessor.kt
@@ -1,9 +1,10 @@
+import com.google.devtools.ksp.processing.CodeGenerator
 import com.google.devtools.ksp.processing.Resolver
 import com.google.devtools.ksp.processing.SymbolProcessor
 import com.google.devtools.ksp.symbol.KSAnnotated
 
-class SymbolProcessorImpl : SymbolProcessor {
+class PrinterProcessor(private val codeGenerator: CodeGenerator) : SymbolProcessor {
     override fun process(resolver: Resolver): List<KSAnnotated> {
-        TODO("Not yet implemented")
+        return emptyList()
     }
 }
