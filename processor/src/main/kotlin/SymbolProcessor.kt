17ca65a454a22ff68bdeb7136e731f444cde08e5
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-11-03 15:54:29 +0100

    feat(processor): created ksp package with its gradle dependencies

---
+++ processor/src/main/kotlin/SymbolProcessor.kt
@@ -0,0 +1,9 @@
+import com.google.devtools.ksp.processing.Resolver
+import com.google.devtools.ksp.processing.SymbolProcessor
+import com.google.devtools.ksp.symbol.KSAnnotated
+
+class SymbolProcessorImpl : SymbolProcessor {
+    override fun process(resolver: Resolver): List<KSAnnotated> {
+        TODO("Not yet implemented")
+    }
+}
